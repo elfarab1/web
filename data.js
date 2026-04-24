5494aa72df2bd6a8d5229352204867fdd7ca3be5
masabi <rumah.masabi@gmail.com>
2026-04-24 09:23:21 +0700

    uplad file pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_meretas": {
+        judul: "sistem pos untuk pt JMC IT Consultant",
+        keahlian: ["IT"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBiL5-alKaw0FMcSGX5Lq2q64rWt66Dj5ouWukAzR6Ng&s&ec=121643124",
+        latar: "ini adalah proses menjaga keamanan data",
+        tantangan: "menjaga setiap sistem keamanan dari seorang meretas data yang menggangu sistem kemanan tersebut.",
+        pelajaran: "menggunakan skil ini untuk hal yang positif",
+        link: ""
+    },
+};
